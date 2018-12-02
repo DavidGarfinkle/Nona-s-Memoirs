492ccf531b51237ca39ae0e953c1fa295e033aad
DavidGarfinkle <david.etan.garfinkle@gmail.com>
2018-12-02 17:54:14 -0500

    New: rough dialogue formatter

--- fmt.py
+++ fmt.py
@@ -2,9 +2,9 @@ import nltk
 nltk.download('punkt', quiet=True)
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 
-def newline_by_sentence(text):
-    tokenizer = PunktSentenceTokenizer()
+tokenizer = PunktSentenceTokenizer()
 
+def newline_by_sentence(text):
     # Clear the current new lines
     text.replace('\n', '')
 
@@ -12,3 +12,15 @@ def newline_by_sentence(text):
     sentences = tokenizer.tokenize(text)
 
     return '\n'.join(sentences)
+
+def newline_surround_dialogue(text):
+    import re
+
+    # Single quotes
+    formatted_text = re.sub(r"'(\w+)'", r"\n\1\n")
+
+    # Double quotes
+    formatted_text = re.sub(r""(\w+)"", r"\n\1\n")
+
+
+#TODO replace "(w+)" with ``($1)''
